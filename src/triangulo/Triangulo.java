b42de65c76a8a3d692f005555aed81a2f77fc1e6
dieguischa <dieguischa@192.168.0.13>
2016-02-29 20:26:03 -0500

    Programa funcional (Por testear)

--- src/triangulo/Triangulo.java
+++ src/triangulo/Triangulo.java
@@ -1,17 +1,28 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package triangulo;
 
+package triangulo;
+import java.lang.Math;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 /**
  *
  * @author Estudiante
  */
 public class Triangulo {
+    
     private Punto [] puntos; 
     private Color [] colores; 
+    private double perimetro; 
+    private double altura; 
+    private double area; 
+    private double base; 
+    private int puntosRegistrados=0; 
+    private int coloresRegistrados=0; 
 
     public Triangulo() {
         
@@ -20,19 +31,87 @@ public class Triangulo {
         
     }
     
-    public void registrarPuntos(int x, int y){
-       
-        for (int i = 0; i < 3; i++) {
-          this.puntos[i]=new Punto(x,y); 
-        }
+    public void registrarPuntos(double x, double y){
+        if(this.puntosRegistrados<3){
+          this.puntos[this.puntosRegistrados]=new Punto(x,y); 
+          this.puntosRegistrados++;
         
+    }else{
+        System.out.println("Puntos Registrados");
+        }
     }
     
      public void registrarColores(int r, int g, int b){
-       
-        for (int i = 0; i < 2; i++) {
-          this.colores[i]=new Color(r,g, b); 
+        if(r<0||r>225||b<0||b>225||g<0||g>225){
+            System.out.println("Datos invalidos");
+        }else{
+        if(this.coloresRegistrados<2){ 
+          this.colores[this.coloresRegistrados]=new Color(r,g, b); 
+        
+     }else{
+       System.out.println("Colores Registrados");     
         }
+     }}
+        
+    
+    public double calcularDistancia(double a, double b, double c, double d){
+        
+        double distancia=sqrt(pow((c-d),2)+pow(a-b,2));
+        
+        return distancia;
+    }
+    
+     
+    public void calcularPerimetro(){
+    
+       Punto p1= this.puntos[0];
+       Punto p2= this.puntos[1];
+       Punto p3= this.puntos[2];
+       this.perimetro=(calcularDistancia(p2.getY(),p1.getY(),p2.getX(),p1.getX())+calcularDistancia(p3.getX(),p2.getX(),p3.getY(),p2.getY())+
+              calcularDistancia(p1.getX(), p3.getX(), p1.getY(), p3.getY()));  
+           
+    }
+    
+    public void calcularBase(){
+        Punto p1=this.puntos[0];
+        Punto p2=this.puntos[1];
+        double base=(calcularDistancia(p2.getX(),p1.getX(),p2.getY(),p1.getY()));
+        this.base=base;
+    }
+    
+    public void calcularAltura(){
+        Punto p1= this.puntos[0];
+        Punto p2= this.puntos[1];
+        Punto p3= this.puntos[2];
+        double semiPerimetro=(this.perimetro/2);
+        this.altura=(2/this.base)*(sqrt(semiPerimetro*(semiPerimetro-this.base)*(semiPerimetro-calcularDistancia(p3.getX(),p2.getX(),p3.getY(),p2.getY()))*(semiPerimetro-
+                 calcularDistancia(p1.getX(), p3.getX(), p1.getY(), p3.getY()))));
         
     }
+    public void calcularArea(){
+        this.area=(this.base*this.altura)/2;
+    }
+
+    public int getPuntosRegistrados() {
+        return puntosRegistrados;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getBase() {
+        return base;
+    }
+    
+    
+    
 }
--- src/triangulo/Triangulos.java
+++ src/triangulo/Triangulos.java
@@ -15,7 +15,70 @@ public class Triangulos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.util.Scanner lectura= new java.util.Scanner(System.in);
+        Triangulo t1= new Triangulo();
+        int eleccion=1;
+        do{
+        System.out.println("Ingrese: "
+                + "\n1. Para registrar punto"
+                + "\n2. Para registrar color"
+                + "\n3. Para conocer el area del triangulo"
+                + "\n4. Para conocer la altura del triangulo"
+                + "\n5. Para conocer el perimetro del triangulo"
+                + "\n0 Para terminar el programa");
+        eleccion= lectura.nextInt();
+        switch (eleccion){
+            case(1):
+            System.out.println("Ingrese coordenada x: ");
+            double x= lectura.nextDouble();
+            System.out.println("Ingrese coordenada y: ");
+            double y= lectura.nextDouble();
+            t1.registrarPuntos(x, y);
+            if(t1.getPuntosRegistrados()==3){
+                t1.calcularBase();
+                System.out.println(t1.getBase());
+                t1.calcularPerimetro();
+                
+                t1.calcularAltura();
+                t1.calcularArea();
+            }
+            break;
+            
+            case(2):
+            System.out.println("Ingrese r: ");
+            int r= lectura.nextInt();
+            System.out.println("Ingrese b: ");
+            int b= lectura.nextInt();
+            System.out.println("Ingrese g: ");
+            int g= lectura.nextInt();
+            t1.registrarColores(r, g, b);   
+            break;
+            case(3):
+            if(t1.getPuntosRegistrados()<3){
+                System.out.println("Ingrese los puntos completos e intente nuevamente");
+            }else{
+                
+                System.out.println(t1.getArea());
+            }
+            break;
+            case(4):
+            if(t1.getPuntosRegistrados()<3){
+                System.out.println("Ingrese los puntos completos e intente nuevamente");
+            }else{
+                
+                System.out.println(t1.getAltura());
+            }
+            break;
+            case(5):
+            if(t1.getPuntosRegistrados()<3){
+                System.out.println("Ingrese los puntos completos e intente nuevamente");
+            }else{
+               
+                System.out.println(t1.getPerimetro());
+            }
+            break; 
+        }
+        }while (eleccion!=0);
     }
     
 }
